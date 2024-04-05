647b3d5eb19f3c012a343fa28bc133d63da50692
Vivek <vivek@koloapp.in>
2024-04-05 14:35:44 +0530

    Renamed plugin Name

--- txn.go
+++ txn.go
@@ -7,21 +7,21 @@ import (
 
 const pluginName = "gorm:txn-plugin"
 
-type Txn struct {
+type GormTxnPlugin struct {
 	*gorm.DB
 }
 
-func (txn *Txn) Name() string {
+func (txn *GormTxnPlugin) Name() string {
 	return pluginName
 }
 
-func (txn *Txn) Initialize(db *gorm.DB) error {
+func (txn *GormTxnPlugin) Initialize(db *gorm.DB) error {
 	txn.DB = db
 	txn.registerCallbacks(db)
 	return nil
 }
 
-func (txn *Txn) registerCallbacks(db *gorm.DB) {
+func (txn *GormTxnPlugin) registerCallbacks(db *gorm.DB) {
 	txn.Callback().Create().Before("*").Register(pluginName, txn.beginTxnIfRequired)
 	txn.Callback().Update().Before("*").Register(pluginName, txn.beginTxnIfRequired)
 	txn.Callback().Delete().Before("*").Register(pluginName, txn.beginTxnIfRequired)
@@ -30,7 +30,7 @@ func (txn *Txn) registerCallbacks(db *gorm.DB) {
 	txn.Callback().Row().Before("*").Register(pluginName, txn.beginTxnIfRequired)
 }
 
-func (txn *Txn) beginTxnIfRequired(db *gorm.DB) {
+func (txn *GormTxnPlugin) beginTxnIfRequired(db *gorm.DB) {
 	ctx := db.Statement.Context
 	if isTxn(ctx) {
 		if txn := getDB(ctx); txn == nil {
